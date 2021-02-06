2365e14259009fa3d35023c62d95c40c0663a957
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-02-06 17:32:13 -0500

    location coordinates

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,22 +1,58 @@
-import React from 'react'
-import { Button, View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
 
-function HomeScreen({navigation}){
-    return(
-        <View style={styles.container}>
-            <Text>GPS MAP</Text>
-            <Button title='Click me!' onPress={() => navigation.navigate('List')}/>
-        </View>
-    );
+function HomeScreen() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        setErrorMsg(
+          'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+        );
+        return;
+      }
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Drexel Location:</Text>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff'
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
+
 
 export default HomeScreen;
