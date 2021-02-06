8d8a97c35b934e2c548d7cd228a14c6ce73bbafd
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2021-02-06 13:55:38 -0500

    test commit 2, messed up react navigation and did git checkout

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Test commit</Text>
+      <Text>Test Commit #2 : </Text>
       <StatusBar style="auto" />
     </View>
   );
